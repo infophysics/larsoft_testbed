f4fa79ea037ea99d1d30b2843ffa4d803175112c
Nick Carrara <ncarrara.physics@gmail.com>
2022-02-16 07:16:13 -0800

    some changes

--- scripts/setup_testbed.sh
+++ scripts/setup_testbed.sh
@@ -9,6 +9,14 @@ done
 export LARSOFT_TESTBED_DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )/../"
 
 INSTALL_DIRECTORY=/dune/app/users/$USER/LArSoftTestbedInstall
+LARSOFT_VERSION=v09_31_00
+DUNETPC_VERSION=$LARSOFT_VERSION
+ARTG4TK_VERSION=v10_03_00
+QUALS=e20:prof
+
+source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
+setup larsoft $LARSOFT_VERSION -q $QUALS
+setup ninja
 
 cd $INSTALL_DIRECTORY
 source localProducts*/setup
